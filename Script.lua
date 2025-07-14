69eb7eaa3450abe29dfbfd5ccdb9cc95298b4563
IamNotXL <131154517+IamNotXL@users.noreply.github.com>
2025-07-14 23:51:05 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "🦑 Ink Game" .. Fluent.Version,
+    Title = "🦑 Ink Game",
     SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
@@ -30,29 +30,151 @@ do
         Title = "Hello, " .. game.Players.LocalPlayer.Name .. "!"
     })
 
-    local executor = detectExecutor()
+    local executor = identifyexecutor()
     
     Tabs.Main:AddParagraph({
-        Title = string.format("Executor: `%s`", executor)
+        Title = string.format("Executor: %s", executor)
     })
+
+    local placeversion = game.PlaceVersion
+
+    Tabs.Main:AddParagraph({
+        Title = "Game Version: " .. placeversion
+    })
+
+    local jobid = game.JobId
     
     Tabs.Main:AddButton({
-        Title = "Button",
-        Description = "Very important button",
+        Title = "Copy Job Id",
         Callback = function()
-            print("Button clicked.")
+            setclipboard(jobid)
+        end
+    })
+
+    local teleportService = game:GetService("TeleportService")
+    
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Join By Job Id",
+        Placeholder = "Enter Job Id",
+        Numeric = false, -- Only allows numbers
+        Finished = true, -- Only calls callback when you press enter
+        Callback = function(Value)
+            teleportService:TeleportToPlaceInstance(game.PlaceId, Value)
+        end
+    })
+
+    local placeId = game.PlaceId
+    
+    Tabs.Main:AddButton({
+        Title = "Rejoin",
+        Callback = function()
+            teleportService:Teleport(placeId, game.Players.LocalPlayer)
+        end
+    })
+
+    local Join = loadstring(game:HttpGet("https://raw.githubusercontent.com/EpicPug/Stuff/main/hop.lua"))()
+    
+    Tabs.Main:AddButton({
+        Title = "Server Hop",
+        Callback = function()
+            Join()
         end
     })
 
 -------------------------------
 
-    Tabs.Player:AddButton({
-        Title = "Button",
-        Description = "Very important button",
-        Callback = function()
-            print("Button clicked.")
+    local Input = Tabs.Player:AddInput("Input", {
+        Title = "Walk Speed",
+        Placeholder = "Value",
+        Numeric = true, -- Only allows numbers
+        Finished = true, -- Only calls callback when you press enter
+        Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
         end
     })
+
+    local Input = Tabs.Player:AddInput("Input", {
+        Title = "Jump Power",
+        Placeholder = "Value",
+        Numeric = true, -- Only allows numbers
+        Finished = true, -- Only calls callback when you press enter
+        Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+        end
+    })
+
+    local Toggle = Tabs.Player:AddToggle("MyToggle", {Title = "Auto Run", Default = false })
+
+    Toggle:OnChanged(function(v)
+        game:GetService("Players").LocalPlayer.AutoRun.Value = v
+    end)
+
+    local Toggle = Tabs.Player:AddToggle("MyToggle", {Title = "Infinite Jump", Default = false })
+
+    Toggle:OnChanged(function(v)
+        _G.infinjump = v
+
+        if _G.infinJumpStarted == nil then
+    	--Ensures this only runs once to save resources
+    	_G.infinJumpStarted = true
+	
+    	--Notifies readiness
+    	game.StarterGui:SetCore("SendNotification", {Title="WeAreDevs.net"; Text="The WeAreDevs Infinite Jump exploit is ready!"; Duration=5;})
+
+    	--The actual infinite jump
+    	local plr = game:GetService('Players').LocalPlayer
+    	local m = plr:GetMouse()
+    	m.KeyDown:connect(function(k)
+		if _G.infinjump then
+			if k:byte() == 32 then
+    			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+    			humanoid:ChangeState('Jumping')
+    			wait()
+    			humanoid:ChangeState('Seated')
+        			end
+        		end
+        	end)
+        end
+    end)
+
+    local Toggle = Tabs.Player:AddToggle("MyToggle", {Title = "Noclip", Default = false })
+
+    Toggle:OnChanged(function(v)
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local function onTouched(part)
+    if not part:IsA("BasePart") then return end
+    if not part.Anchored then return end
+    if not part.CanCollide then return end
+
+    local character = LocalPlayer.Character
+    if not character then return end
+
+    local hrp = character:FindFirstChild("HumanoidRootPart")
+    if not hrp then return end
+
+    -- Prevent disabling floors
+    if part.Position.Y < (hrp.Position.Y - hrp.Size.Y) then return end
+
+    -- Passed all checks
+    part.CanCollide = v
+end
+
+-- Wait for character and hook HRP
+local function setup()
+    local char = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+    local hrp = char:WaitForChild("HumanoidRootPart")
+
+    hrp.Touched:Connect(onTouched)
+end
+
+if LocalPlayer.Character then
+    setup()
+end
+
+LocalPlayer.CharacterAdded:Connect(setup)
+    end)
     
 -------------------------------
     
